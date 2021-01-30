1d44d14024057a7fd48e3cefe530256df8314000
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-01-30 15:13:50 +0300

    pokemon detay reducer,action yazıldı ve sayfası olusturuldu

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,11 @@
 import {combineReducers} from "redux";
 import pokemonListReducer from "./pokemonListReducer";
+import pokemonMultipleReducer from  "./PokemonMultipleReducer";
+
+
 const rootReducer = combineReducers({
     pokemonListReducer,
+    pokemonMultipleReducer
 });
 
 export default rootReducer;
